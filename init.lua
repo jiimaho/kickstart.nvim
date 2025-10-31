f88da723746221129d6b3c3a464a6e6f5c4125d3
Jim Aho <jimaho@MacBook-Pro-som-tillhor-Jim.local>
2025-10-31 09:42:20 +0100

    feat: enable oil

--- init.lua
+++ init.lua
@@ -811,7 +811,17 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
   { -- Autocompletion
     'saghen/blink.cmp',
     event = 'VimEnter',
@@ -1076,3 +1086,4 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+require('oil').setup()
