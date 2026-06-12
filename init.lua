b7e5b2509f49dd1dafada23bcfa77328da92d5ab
Jim Aho <jim.aho@hedinit.com>
2026-06-12 08:41:07 +0200

    fix(treesitter): migrate to main branch for nvim 0.12+
    The master branch is archived and breaks on nvim 0.12+. The main branch
    no longer auto-attaches, so highlighting and indentation are enabled per
    buffer via a FileType autocmd. Also adds c_sharp, razor, json, xml and
    yaml parsers.

--- init.lua
+++ init.lua
@@ -1117,28 +1117,51 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main', -- the old 'master' branch is archived and breaks on nvim 0.12+
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    lazy = false,
+    config = function()
+      local ts = require 'nvim-treesitter'
+      ts.setup {}
+
+      local ensure_installed = {
+        'bash',
+        'c',
+        'c_sharp',
+        'diff',
+        'html',
+        'json',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'razor',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
+      }
+      ts.install(ensure_installed)
+
+      -- The main branch no longer auto-attaches: enable highlighting (and
+      -- indentation where indent queries exist) per buffer.
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('treesitter-attach', { clear = true }),
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(args.match)
+          if not lang then
+            return
+          end
+          if not pcall(vim.treesitter.start, args.buf, lang) then
+            return
+          end
+          if vim.treesitter.query.get(lang, 'indents') then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
   },
   {
     'nvim-telescope/telescope-file-browser.nvim',
